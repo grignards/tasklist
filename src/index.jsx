3d961ae45656fe524201202f0840c31e506ad303
Marie Begue <marie.prandstetter@gmail.com>
2021-03-24 20:53:29 +0100

    to be continued

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+// todo
+// When a user types a query, the list of Gifs should display the 10 first
+ // results the giphy-api returns.
+// When a user clicks on a gif from the list, it should appear on the main
+// scene on the left.
+
 import '../assets/stylesheets/application.scss';
 
 const Hello = ({ name }) => {
@@ -16,3 +22,9 @@ const root = document.getElementById('root');
 if (root) {
   ReactDOM.render(<Hello name="World" />, root);
 }
+
+// needed components :
+// search bar
+// displayed gif = result
+// list of gifs
+//
