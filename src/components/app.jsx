f9b050e00e1e3e8903e54ea74ad7bdf10a099fd2
Marie Begue <marie.prandstetter@gmail.com>
2021-03-30 12:24:00 +0200

    to be continued 1:17

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -5,38 +5,45 @@ import SearchBar from './search_bar.jsx';
 import Gif from './gif.jsx';
 import GifList from './gif_list.jsx';
 
+const GIPHY_API_KEY = 'ZPdqW81cOzqfkPUPdFNVkkj2m8TVdmkN';
+
 class App extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      gifList: [],
+      gifs: [],
       selectedGifId: "6Hhrt25GQrd1YDR1Bw"
     }
+    this.search("homer");
   }
 
-  search = (query) => {
-    giphy('ZPdqW81cOzqfkPUPdFNVkkj2m8TVdmkN')
-    // implement search function
-  }
+  // search(query) {
+  //   const giphEndpoint = `https://api.giphy.com/v1/gifs/search?api_key=${GIPHY_API_KEY}&q=${query}&limit=10`
+  //   fetch(giphEndpoint).then(response => response.json()).then((data) => {
+  //     const gifs = data.data.map(giph => giph.id)
+  //     this.setState({
+  //       gifs: gifs
+  //     })
+  //   })
+  // }
 
 
   render() {
     const gifs = [
-      { id: "AEkWi5dH0OieoqXJj3"}, // pourquoi cette var ne fonctionne pas dans GifList ?
-      { id: "6Hhrt25GQrd1YDR1Bw"}
+      { id: "6Hhrt25GQrd1YDR1Bw" },
+      { id: "6Hhrt25GQrd1YDR1Bw" },
     ];
-
     return (
       <div>
         <div className="left-scene">
-          <SearchBar />
+          <SearchBar searchFunction={this.search} />
           <div className="selected-gif">
-            <Gif id={this.state.selectedGifId}/>
+            <Gif id={this.state.selectedGifId} />
           </div>
         </div>
         <div className="right-scene">
-          <GifList gifs={this.state.gifList} />
+          <GifList gifs={this.state.gifs} />
         </div>
       </div>
     );
--- src/components/gif_list.jsx
+++ src/components/gif_list.jsx
@@ -3,13 +3,13 @@ import Gif from './gif.jsx';
 
 class GifList extends Component {
   renderList = () => {
-    return this.props.gifs.map(gif => <Gif id={gif.id} key={gif.id} />)
+    return this.props.gifs.map(gif => <Gif id={gif.id} key={gif.id} />);
   }
 
   render() {
     return (
       <div className="gif-list">
-          {this.renderList()}
+        {this.renderList()}
       </div>
     );
   }
