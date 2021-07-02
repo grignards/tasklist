a7123c85dd58534e2b1fde540a987f2c9d9ee9c8
Marie Bégué <marie.begue@algoo.fr>
2021-07-02 10:05:08 +0200

    wip submit btn

--- src/components/new_task.jsx
+++ src/components/new_task.jsx
@@ -14,14 +14,20 @@ class NewTask extends React.Component {
     });
   }
 
+  handleSubmit = e => {
+    e.preventDefault();
+    alert("submit");
+    // or you can send to backend
+  };
+
   render () {
     return (
       <div>
         <input type='text' value={this.state.newTaskLabel} onChange={this.handleChangeLabel} />
         <button
-          type='button'
-          // onClick={this.props.onClickSubmit} // par reference
-          onClick={e => this.props.onClickSubmit(this.state.newTaskLabel)}
+          type='submit'
+          onClick={this.handleSubmit}
+          // onClick={e => this.props.onClickSubmit(this.state.newTaskLabel)}
         >
           Submit
         </button>
@@ -30,3 +36,36 @@ class NewTask extends React.Component {
   }
 }
 export default NewTask;
+
+//////////////////////////////////////////////////////:
+
+// import React from 'react';
+
+// class NewTask extends React.Component {
+//   constructor (props) {
+//     super(props);
+//     this.state = {
+//       newTaskLabel: '',
+//     };
+//   }
+
+// handleChangeLabel = (e) => {
+//   this.setState({
+//     newTaskLabel: e.target.value
+//   });
+// }
+
+// onFormSubmit = e => {
+//   this.props.onClickSubmit(this.state.newTaskLabel)
+// }
+
+//   render () {
+//     return (
+//       <div>
+//         <input type='text' value={this.state.newTaskLabel} onChange={this.handleChangeLabel} />
+//           <button type="submit" onSubmit={this.onFormSubmit}>Submit</button>
+//       </div>
+//     );
+//   }
+// }
+// export default NewTask;
